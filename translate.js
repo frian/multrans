20ed8241f1dacdf291d219f759ec834f2ad7009b
frian <a@frian.org>
2018-11-17 18:56:23 +0100

    added set command

--- translate.js
+++ translate.js
@@ -2,36 +2,62 @@
 
 /*jshint esversion: 6 */
 
-const conf = {
-    "defaultFromLang": 'fr',    // en|fr|de|...
-    "defaultToLangs" : 'it,en',    // de|en|fr,de|fr,de,it
-};
-
-
+const fs = require('fs');
 const path = require('path');
-const args = require('commander');
+const program = require('commander');
 const googleTranslateApi = require('@k3rn31p4nic/google-translate-api');
 
+const conf = require('./config');
 const version = require('./package.json').version;
 
 
-args
+program
     .usage('[options] string_to_translate')
     .description('translate to one or more languages')
     .option('-f, --from <lang>', 'language of input string', conf.defaultFromLang)
     .option('-t, --to <lang>', 'languages to translate to', conf.defaultToLangs)
-    .option('-v, --verbose')
+    .option('-v, --verbose');
+
+program
+    .command('set')
+    .description('view and edit default in and out lang(s). (-h for help)')
+    .option("-i, --input <lang>", "Set input lang")
+    .option("-o, --output <lang(s)>", "Set output lang(s)")
+    .action(function(options) {
+        if (options.input === undefined && options.output === undefined) {
+            console.log("default input lang :     " + conf.defaultFromLang);
+            console.log("default output lang(s) : " + conf.defaultToLangs);
+            process.exit(0);
+        }
+        else if (options.input) {
+            console.log('set input lang to : ' + options.input);
+            conf.defaultFromLang = options.input;
+            let newConf = JSON.stringify(conf, null, 4);
+            fs.writeFileSync('config.json', newConf);
+            process.exit(0);
+        }
+        else if (options.output) {
+            console.log('set output lang(s) to ' + options.output);
+            conf.defaultToLangs = options.output;
+            let newConf = JSON.stringify(conf, null, 4);
+            fs.writeFileSync('config.json', newConf);
+            process.exit(0);
+        }
+    });
+
+program
     .version(version)
     .parse(process.argv);
 
 
-if (! args.args.length) {
-  args.help();
+if (! program.args.length) {
+  program.help();
 }
 
-const fromLang = args.from || conf.defaultFromLang;
 
-const toLangs = args.to.split(',') || conf.defaultToLangs.split(',');
+const fromLang = program.from || conf.defaultFromLang;
+
+const toLangs = program.to.split(',') || conf.defaultToLangs.split(',');
 
 
 // -- check if we have a same language in --from and --to
@@ -40,7 +66,7 @@ if (toLangs.includes(fromLang)) {
     process.exit(0);
 }
 
-const verbose = args.verbose;
+const verbose = program.verbose;
 
 // -- flag for printResult
 let multiTrans = false;
@@ -50,7 +76,7 @@ if (toLangs.length > 1) {
 }
 
 
-const toTranslate = args.args[0];
+const toTranslate = program.args[0];
 
 
 // -- flag ; we want ro check this only once
@@ -72,7 +98,10 @@ let reverseTranslation = '';
 
 translate(toTranslate, fromLang, toLangs[0])
     .then((result) => {
-        checkTranslation = result.text;
+        return new Promise(function(resolve, reject) {
+            checkTranslation = result.text;
+            resolve(checkTranslation);
+        });
     })
     .then(() => {
         return translate(checkTranslation, toLangs[0], fromLang).then((result) => {
